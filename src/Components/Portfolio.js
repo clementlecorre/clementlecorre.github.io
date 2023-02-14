6ca8dd2d528acb42136d131e953a9adc4f50f709
clement <clement@le-corre.eu>
2023-02-14 16:55:35 +0100

    fix: bad routing for blogpost

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Portfolio(props) {
   if (props.data) {
-    var projects = props.data.projects.map(function (projects) {
-      var projectImage = 'images/portfolio/' + projects.image;
-      return <div key={projects.title} className="columns portfolio-item">
-        <div className="item-wrap">
-          <a href={projects.url} title={projects.title}>
-            <img alt={projects.title} src={projectImage} />
-            <div className="overlay">
-              <div className="portfolio-item-meta">
-                <h5>{projects.title}</h5>
-                <p>{projects.category}</p>
+    var projects = props.data.projects.map(function (project) {
+      const projectImage = 'images/portfolio/' + project.image;
+      const linkProps = project.url_internal ? { to: project.url } : { href: project.url };
+      return (
+        <div key={project.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <Link title={project.title} {...linkProps}>
+              <img alt={project.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{project.title}</h5>
+                  <p>{project.category}</p>
+                </div>
               </div>
-            </div>
-            <div className="link-icon"><i className="fa fa-link"></i></div>
-          </a>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </Link>
+          </div>
         </div>
-      </div>
-    })
+      );
+    });
   }
 
   return (
     <section id="portfolio">
-
       <div className="row">
-
         <div className="twelve columns collapsed">
-
           <h1>Jetez un coup d'œil à certains de mes projets.</h1>
-
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
             {projects}
           </div>
